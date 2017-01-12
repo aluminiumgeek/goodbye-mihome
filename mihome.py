a4c842406e337d4dc2752fb1393507da3e51f1d9
Mikhail Mezyakov <mihail265@gmail.com>
2017-01-12 06:36:36 +0400

    Use redis to store values

--- mihome.py
+++ mihome.py
@@ -7,6 +7,7 @@ import time
 from Crypto.Cipher import AES
 from datetime import datetime
 from threading import Thread
+from redis import StrictRedis
 
 import config
 from plugins import sensor_ht, gateway_token
@@ -14,6 +15,7 @@ from web.w import run_app
 
 conn = psycopg2.connect("dbname={} user={} password={}".format(config.DBNAME, config.DBUSER, config.DBPASS))
 cursor = conn.cursor()
+store = StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
  
 MULTICAST_PORT = 9898
  
@@ -22,11 +24,9 @@ SOCKET_BUFSIZE = 1024
 
 IV = bytes([0x17, 0x99, 0x6d, 0x09, 0x3d, 0x28, 0xdd, 0xb3, 0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58, 0x56, 0x2e])
 CIPHER = AES.new(config.MIHOME_GATEWAY_PASSWORD, AES.MODE_CBC, IV)
-gateway_addr, token, sid = None, None, None
 
 
 def receiver():
-    global gateway_addr, token, sid
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", MULTICAST_PORT))
  
@@ -39,8 +39,8 @@ def receiver():
     current = {}
 
     while True:
-        data, addr = sock.recvfrom(SOCKET_BUFSIZE)  # buffer size is 1024 bytes
-        gateway_addr = addr
+        data, (addr, _) = sock.recvfrom(SOCKET_BUFSIZE)  # buffer size is 1024 bytes
+        store.set('gateway_addr', addr)
         print(datetime.now().isoformat(), data)
         message = json.loads(data.decode())
         data = json.loads(message['data'])
@@ -51,17 +51,19 @@ def receiver():
             if not result:
                 continue
             sid, token = result
+            store.set('gateway_sid', sid)
+            store.set('gateway_token', token)
             continue
         current = {}
 
 
 def send_command(command):
-    global gateway_addr
+    gateway_addr = store.get('gateway_addr')
     if gateway_addr is None:
         print("Doesn't receive any heartbeat from gateway. Delaying request for 10 seconds.")
         time.sleep(10)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.connect((gateway_addr[0], MULTICAST_PORT))
+    sock.connect((gateway_addr, MULTICAST_PORT))
     sock.send(json.dumps(command).encode('ascii'))
     data, addr = sock.recvfrom(SOCKET_BUFSIZE)
     sock.close()
@@ -70,9 +72,7 @@ def send_command(command):
 
 def get_key():
     """Get current gateway key"""
-    global CIPHER, token
-
-    encrypted = CIPHER.encrypt(token)
+    encrypted = CIPHER.encrypt(store.get('gateway_token'))
     return binascii.hexlify(encrypted)
 
 
